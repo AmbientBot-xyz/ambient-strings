aa86895a38677d358a003c6d8be160748a09c9e0
Krzysztof Matuszak <kmatuszak04@icloud.com>
2022-08-22 04:24:33 +0200

    🌐 Updated core strings

--- core/en.ts
+++ core/en.ts
@@ -1,28 +1,30 @@
 export default {
   global: {
     notEnabled:
-      "It looks like you are trying to use the bot on a group that has not been added to the allowed list in the <c>Ambient management panel</c>. Ask bot administrator to add a group or create your own bot completely free on https://www.ambientbot.xyz/",
+      "It looks like you are trying to use the bot on a group that has not been added to the allowed list in the <c>Ambient management panel</c>. Ask bot administrator to add a group or create your own bot completely free on {0}",
     properBotIssues: [
-      "It looks like you are trying to use a bot that is not working properly. Information about the bot enabled on this group:\n", //
+      "It looks like you are trying to use a bot that is not working properly. Information about the bot enabled on this group:\n",
       "🤖 Bot name: <c>{0}</c>",
       "🪪 Account: <c>{1}</c>",
     ],
     wrongBotMentioned: [
-      "It looks like you are trying to use the wrong bot. Information about the bot enabled on this group:\n", //
+      "It looks like you are trying to use the wrong bot. Information about the bot enabled on this group:\n",
       "🤖 Bot name: <c>{0}</c>",
       "🪪 Account: <c>{1}</c>",
     ],
-    disabledManual: "❌ Bot <c>{0}</c> has been disabled on this group by the administrator!",
-    disabledAuto: "❌ Bot <c>{0}</c> has been disabled on this group because it has been added to allowed groups in another bot.",
-    enabled: "✅ Bot <c>{0}</c> has been enabled on this group, all members of the group can now use it.",
+    disabledManual: "♿ Bot <c>{0}</c> has been disabled on this group by the administrator!",
+    disabledAuto: "💡 Bot <c>{0}</c> has been disabled on this group because it has been added to allowed groups in another bot.",
+    enabled: "🔘 Bot <c>{0}</c> has been enabled on this group, all members of the group can now use it.",
+    threadJailed: "⏳ Operations are currently being performed in this group that prevent your message from being processed.",
+    eventError: "💀 An unknown error occurred while trying to process your message. Please try again later and if the error persists, notify us on the Discord server: {0}",
   },
   commands: {
     bot: [
-      "🤖 Info about Ambient:\n", //
+      "ℹ Ambient info:\n",
       "🪁 Core version: <c>v{0}+{1}/{2}</c>",
       "🫡 Instance: <c>{3}</c>",
       "⏳ Instance uptime: <c>{4}</c>",
-      "⏳ Bot uptime: <c>{5}</c>",
+      "🤖 Bot uptime: <c>{5}</c>",
       "🪁 Environment: <c>v{6}+{7}/{8}</c>",
       "🍒 Creation date: <c>{9}</c>",
       "😼 Author: <c>{10}</c>",
--- core/pl.ts
+++ core/pl.ts
@@ -1,28 +1,31 @@
 export default {
   global: {
     notEnabled:
-      "Wygląda na to, że próbujesz skorzystać z bota na grupie która nie została dodana do listy dozwolonych w <c>panelu zarządzania Ambientem</c>. Poproś administratora bota o dodanie grupy lub stwórz własnego bota całkowicie za darmo na https://www.ambientbot.xyz/",
+      "Wygląda na to, że próbujesz skorzystać z bota na grupie która nie została dodana do listy dozwolonych w <c>panelu zarządzania Ambientem</c>. Poproś administratora bota o dodanie grupy lub stwórz własnego bota całkowicie za darmo na {0}",
     properBotIssues: [
-      "Wygląda na to, że próbujesz skorzystać z bota który nie działa prawidłowo. Informacje o bocie włączonym na tej grupie:\n", //
+      "Wygląda na to, że próbujesz skorzystać z bota który nie działa prawidłowo. Informacje o bocie włączonym na tej grupie:\n",
       "🤖 Nazwa bota: <c>{0}</c>",
       "🪪 Konto: <c>{1}</c>",
     ],
     wrongBotMentioned: [
-      "Wygląda na to, że próbujesz skorzystać z niewłaściwego bota. Informacje o bocie włączonym na tej grupie:\n", //
+      "Wygląda na to, że próbujesz skorzystać z niewłaściwego bota. Informacje o bocie włączonym na tej grupie:\n",
       "🤖 Nazwa bota: <c>{0}</c>",
       "🪪 Konto: <c>{1}</c>",
     ],
-    disabledManual: "❌ Bot <c>{0}</c> został wyłączony na tej grupie przez administratora!",
-    disabledAuto: "❌ Bot <c>{0}</c> został wyłączony na tej grupie, ponieważ została ona dodana do dozwolonych grup w innym bocie.",
-    enabled: "✅ Bot <c>{0}</c> został włączony na tej grupie, wszyscy członkowie grupy mogą z niego teraz korzystać.",
+    disabledManual: "♿ Bot <c>{0}</c> został wyłączony na tej grupie przez administratora!",
+    disabledAuto: "💡 Bot <c>{0}</c> został wyłączony na tej grupie, ponieważ została ona dodana do dozwolonych grup w innym bocie.",
+    enabled: "🔘 Bot <c>{0}</c> został włączony na tej grupie, wszyscy członkowie grupy mogą z niego teraz korzystać.",
+    threadJailed: "⏳ W tej grupie są obecnie wykonywane operacje uniemożliwiające przetworzenie Twojej wiadomości.",
+    eventError:
+      "💀 Wystąpił nieznany błąd podczas próby przetworzenia Twojej wiadomości. Spróbuj ponownie później, a jeśli błąd będzie się powtarzał, powiadom nas na serwerze Discord: {0}",
   },
   commands: {
     bot: [
-      "🤖 Informacje o Ambiencie:\n", //
+      "ℹ Informacje o Ambiencie:\n",
       "🪁 Wersja jądra: <c>v{0}+{1}/{2}</c>",
       "🫡 Instancja: <c>{3}</c>",
       "⏳ Czas działania instancji: <c>{4}</c>",
-      "⏳ Czas działania bota: <c>{5}</c>",
+      "🤖 Czas działania bota: <c>{5}</c>",
       "🪁 Środowisko: <c>v{6}+{7}/{8}</c>",
       "🍒 Data powstania: <c>{9}</c>",
       "😼 Autor: <c>{10}</c>",
